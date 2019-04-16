9ebb4efae45679813dd5fa3eaaaa4a6cb5643f4d
Bitaman <b.amani.s@gmail.com>
2019-04-16 06:09:50 -0700

    added index,show and new actions for passenger

---
+++ app/controllers/passengers_controller.rb
@@ -0,0 +1,35 @@
+class PassengersController < ApplicationController
+  def index
+    @passenger = Passengers.all
+  end
+
+  def show
+    passenger_id = params[:id]
+    passenger = Passenger.find_by(id: passenger_id)
+    unless passenger
+      head :not_found
+      redirect_to main
+    end
+  end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+    unless params["passenger"]
+      render :new,
+             status: bad_request
+      return
+    end
+    @passenger.save
+    redirect_to passenger_path
+  end
+
+  private
+
+  def strong_params
+    return params.require(:task).permit(:name)
+  end
+end
