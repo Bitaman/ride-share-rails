d7602349a988d5882fea1fec3ef3f61f7eaea578
Tatiana Quintana <tatsqui@gmail.com>
2019-04-16 11:36:22 -0700

    added actions to driver and resources to routes

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,60 @@
 class DriversController < ApplicationController
+  def new # form, needs a view
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+
+    @driver.save 
+
+    redirect_to drivers_path
+  end
+
+  def show # shows a view
+    @driver = Driver.find_by(params[:id])
+
+    unless @driver
+      head :not_found
+      #redirect_to root_path
+    end
+    #redirect_to drivers path or root_path?
+  end
+
+  def edit # form, needs a view to update info on a driver
+    @driver = Driver.find_by(params[:id])
+
+    unless @driver
+      head :not_found
+    end
+  end
+
+  def update # takes info from form, doesn't need @
+    driver = Driver.find_by(params[:id])
+
+    unless driver
+      head :not_found
+    end
+
+    driver.update(driver_params)
+
+    # redirect_to driver_path
+  end
+
+  def destroy # removes a driver from list of drivers
+    driver = Driver.find_by(params[:id])
+     
+    unless driver
+      head :not_found
+      return
+    end
+
+    driver.destroy
+
+    # redirect_to drivers_path
+  end
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin)
+  end
 end
