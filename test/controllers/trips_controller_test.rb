38895e006bdb415af1955be981eddb12c44056a9
Tatiana Quintana <tatsqui@gmail.com>
2019-04-18 17:06:33 -0700

    writing tests for trips controller

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -1,23 +1,43 @@
 require "test_helper"
 
 describe "TripsController" do
-  describe "show" do
-    # Your tests go here
+  let(:passenger) { Passenger.all.sample }
+  let(:driver) { Driver.all.sample }
+  describe "create" do
+    # it "creates the generated trip" do
+    #   trip_data = {
+    #     trip: {
+    #       cost: 10,
+    #       date: Time.now,
+    #       price: rand(1.00..300.00).round(2),
+    #       passenger_id: passenger.id,
+    #       driver_id: driver.id,
+    #     },
+    #   }
+
+    #   expect {
+    #     post passenger_trips_path(trip_data[:passenger_id]), params: trip_data
+    #   }.must_change "Trip.count", +1
+    # end
   end
 
   describe "edit" do
-    # Your tests go here
+    it "allows you to edit a trip" do
+    end
   end
 
   describe "update" do
-    # Your tests go here
+    it "# Your tests go here" do
+    end
   end
 
-  describe "create" do
-    # Your tests go here
+  describe "show" do
+    it "# Your tests go here" do
+    end
   end
 
   describe "destroy" do
-    # Your tests go here
+    it "# Your tests go here" do
+    end
   end
 end
